9d0676c16a640d42832f866bbb2516562f92cac7
GallantJohnny <hannsg14@gmail.com>
2020-07-01 10:21:19 +0200

    Folder restructering

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
-const router = require('./routes');
+const router = require('./api/routes');
 
 dotenv.config();
-app.use('/', router);
+app.use('/api/', router);
 
 app.listen(process.env.SERVER_PORT, () => console.log(`Server is running on ${process.env.SERVER_PORT}`));
