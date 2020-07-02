459b9dbf67712b9d386baf4d430f7430c711ceb2
GallantJohnny <hannsg14@gmail.com>
2020-07-02 16:33:43 +0200

    Added body JSON parser

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv');
 const router = require('./api/routes');
 
 dotenv.config();
+// Parse incoming body with json payload
+app.use(express.json());
 app.use('/api/', router);
 
 app.listen(process.env.SERVER_PORT, () => console.log(`Server is running on ${process.env.SERVER_PORT}`));
