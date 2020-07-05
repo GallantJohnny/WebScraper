6a21ff34dc0581d9260b9c3126e1859ea67ce8fa
GallantJohnny <hannsg14@gmail.com>
2020-07-05 10:12:41 +0200

    Preapiring to deploy to Heroku

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ const dotenv = require('dotenv');
 const router = require('./api/routes');
 
 dotenv.config();
+
 // Parse incoming body with json payload
 app.use(express.json());
 app.use('/api', router);
+
+// ... other app.use middleware 
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+// ...
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(process.env.SERVER_PORT, () => console.log(`Server is running on ${process.env.SERVER_PORT}`));
